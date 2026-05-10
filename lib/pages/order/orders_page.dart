d40642668f6769795e0e42029786527475f1fd31
gyyrroo <syauqialghifari51@gmail.com>
2026-05-10 11:24:55 +0700

    fix: update import path for MainAppBar to shared directory

--- lib/pages/order/orders_page.dart
+++ lib/pages/order/orders_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import '../../widgets/main_head_bar.dart';
+import '../../widgets/shared/main_head_bar.dart';
 import '../../widgets/shared/saldo_card.dart';
 import '../../data/dummy_data.dart';
 import '../../widgets/shared/main_bottom_nav_bar.dart';
